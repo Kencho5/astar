758d36c29185c2973a88e5e87ee6420eab076cf5
Kencho5 <giokenchadze@gmail.com>
2023-09-09 18:26:06 +0400

    added window

--- main.cpp
+++ main.cpp
@@ -1,2 +1,19 @@
-// g++ main.cpp -I/opt/homebrew/Cellar/sfml/2.5.1_2/include -o app -L/opt/homebrew/Cellar/sfml/2.5.1_2/lib -lsfml-graphics -lsfml-window -lsfml-system && ./app
+#include </opt/homebrew/Cellar/sfml/2.5.1_2/include/SFML/Graphics.hpp>
 
+int main() {
+  sf::Window window(sf::VideoMode(800, 600), "a*");
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+  }
+
+  return 0;
+}
+
+// g++ main.cpp -I/opt/homebrew/Cellar/sfml/2.5.1_2/include -o app
+// -L/opt/homebrew/Cellar/sfml/2.5.1_2/lib -lsfml-graphics -lsfml-window
+// -lsfml-system && ./app
